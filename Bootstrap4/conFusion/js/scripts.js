c35a46018217b6d92ea0d001d325370141684239
G.F.Pajarinen <NossaCasa@GFMacBook-Pro.local>
2020-07-24 11:20:19 +0300

    js file

---
+++ Bootstrap4/conFusion/js/scripts.js
@@ -0,0 +1,37 @@
+//ONE BUTTON : doesnt work either
+   
+        $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+    
+
+    //not working: FOR 2 BUTTONS 
+    /**
+        $(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 2000 } );
+            
+            $("#carousel-pause").click(function(){
+                $("#mycarousel").carousel('pause');
+            });
+            $("#carousel-play").click(function(){
+                $("#mycarousel").carousel('cycle');
+            });
+        });
+        
+
+    //FOR TOOLTIP
+    
+        $(document).ready(function(){
+            $('[data-toggle= "tooltip"]').tooltip();
+        });
+
+        */
